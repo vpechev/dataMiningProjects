27ea4ff0493e3a0f2c8fb1dab736eda827f0e422
Vladimir Pechev <vladimir.pechev@scalefocus.com>
2016-11-01 07:47:19 +0200

    add excluded classes to git

---
+++ SlidingBlocks/PriorityQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+namespace SlidingBlocks
+{
+    public class PriorityQueue
+    {
+        private OrderedSet<Node> priorityQuery{ get; set; }
+
+        public PriorityQueue()
+        {
+            priorityQuery = new OrderedSet<Node>();
+        }
+
+        public void Enque(Node item)
+        {
+            priorityQuery.Add(item);
+        }
+
+        public Node Deque()
+        {   
+            return priorityQuery.RemoveFirst();
+        }
+
+        public bool IsEmpty()
+        {
+            return priorityQuery.Count == 0;
+        }
+    }
+}
---
+++ SlidingBlocks/SlidingBocksSolver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingBlocks
+{
+    public class SlidingBocksSolver
+    {
+        public static void SolveSlidingBocks(Node root)
+        {
+            PriorityQueue priorityQueue = new PriorityQueue();
+            List<int[,]> visited = new List<int[,]>();
+            priorityQueue.Enque(root);
+
+            Console.WriteLine("The input board is: ");
+            BoardUtil.PrintBoard(root.Board);
+
+            do
+            {
+                Node currentElement = priorityQueue.Deque();
+                visited.Add(currentElement.Board);
+                Console.WriteLine(currentElement.MoveDirection);
+                Console.WriteLine("Current Element Heuristic: {0} ", currentElement.Heuristic);
+                BoardUtil.PrintBoard(currentElement.Board);
+                
+                if (currentElement.Heuristic == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The result board is: ");
+                    BoardUtil.PrintBoard(currentElement.Board);
+                    Console.WriteLine("Total distance: " + currentElement.Distance);
+                    return;
+                }
+                else
+                {
+                    BoardUtil.GenerateChildren(currentElement);
+
+                    foreach (var child in currentElement.ChildrenBoards)
+                    {
+                        if ( !BoardUtil.ContainsVisitedListCertainBoard(visited, child.Board) )
+                        {
+                            priorityQueue.Enque(child);
+                        }
+                    }
+                }
+            }
+            while (!priorityQueue.IsEmpty());
+
+            throw new Exception("This puzzle cannot be solved");
+        }
+    }
+}
