a3a0bf2e0e23fa2f9ceace39513bb089f056f579
vpechev <vlado369@abv.bg>
2016-11-14 20:26:15 +0200

    add missed classes

---
+++ KnapsackProblem/GenericAlgorithmUtil.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnapsackProblem
+{
+    public class GenericAlgorithmUtil
+    {
+        public const int POPULATION_CAPACITY = 10;
+        public const int DISCARD_CELLS_COUNT = 40 * POPULATION_CAPACITY / 100;
+        private static Random random = new Random();
+        private static int KnapsackMaxWeight {get; set;}
+
+        public GenericAlgorithmUtil(int maxWeight)
+        {
+            KnapsackMaxWeight = maxWeight;
+        }
+
+        public void CreateNewGeneration(List<Cell> population)
+        {
+            Select(population);
+            Crossover(population, 0, 1);
+            //Crossover(population, 0, 2);
+            //Crossover(population, 1, 2);
+            Mutate(population);
+        }
+
+        private void Select(List<Cell> population)
+        {
+            DoFitness(population);
+            population.Take(DISCARD_CELLS_COUNT);
+        }
+
+        private void Crossover(List<Cell> population, int firstCellIndex, int secondCellIndex)
+        {
+            var firstParentChromosomes = population[firstCellIndex].Chromosomes;
+            var secondParentChromosomes = population[secondCellIndex].Chromosomes;
+            var randomIndex = random.Next(1, Math.Min(firstParentChromosomes.Count(), secondParentChromosomes.Count()));
+
+            var firstChild = CreateChild(population[firstCellIndex], population[secondCellIndex], randomIndex);
+            var secondChild = CreateChild(population[secondCellIndex], population[firstCellIndex], randomIndex);
+
+            if (firstChild.TotalMi < KnapsackMaxWeight)
+                population.Add(firstChild);
+
+            if (secondChild.TotalMi < KnapsackMaxWeight)
+                population.Add(secondChild);
+        }
+
+        private void Mutate(List<Cell> population)
+        {
+            Random random = new Random();
+            int randInt = random.Next(0, 100);
+
+            if (randInt < 20)
+            {
+                population.Sort();
+                Console.WriteLine("Mutation occured...");
+                var lastItem = population.Last();
+                var chromosomes = lastItem.Chromosomes;
+                var chromosomesLen = chromosomes.Count;
+                SwapChromosomes(chromosomes, chromosomes.Count / 2 - 1 , chromosomes.Count / 2);
+            }
+        }
+
+        private Cell CreateChild(Cell firstParent, Cell secondParent, int randomIndex)
+        {
+            var firstParentChromosomes = firstParent.Chromosomes;
+            var secondParentChromosomes = secondParent.Chromosomes;
+
+            var childChromosomes = new List<Chromosome>();
+            
+            Cell childCell = new Cell()
+            {
+                Chromosomes = childChromosomes,
+                TotalCi = 0,
+                TotalMi = 0,
+
+            };
+
+            // take first half from the first parent
+            for (int i = 0; i < randomIndex; i++)
+            {
+                AddChromosomeToCell(childCell, firstParentChromosomes[i]);
+            }
+
+            // take second half from the second parent
+            var modifiedSecondChromosomesOrder = secondParentChromosomes.GetRange(randomIndex, secondParentChromosomes.Count - randomIndex);
+            modifiedSecondChromosomesOrder.Sort();
+            for (int i = 0; i < modifiedSecondChromosomesOrder.Count; i++)
+            {
+                AddChromosomeToCell(childCell, modifiedSecondChromosomesOrder[i]);
+            }
+
+
+            //add additional chromosomes
+            if (childCell.TotalMi < KnapsackMaxWeight)
+            {
+                var modifiedAdditionalChromosomesOrder = firstParentChromosomes.GetRange(randomIndex, firstParentChromosomes.Count - randomIndex);
+                modifiedAdditionalChromosomesOrder.Sort();
+                for (int i = randomIndex; i < modifiedAdditionalChromosomesOrder.Count(); i++)
+                {
+                    AddChromosomeToCell(childCell, modifiedAdditionalChromosomesOrder[i]);
+
+                    if (childCell.TotalMi == KnapsackMaxWeight)
+                        return childCell;
+                }
+            }
+
+            if (childCell.TotalMi < KnapsackMaxWeight)
+            {
+                var modifiedAdditionalChromosomesOrder = secondParentChromosomes.GetRange(randomIndex, secondParentChromosomes.Count - randomIndex);
+                modifiedAdditionalChromosomesOrder.Sort();
+                for (int i = 0; i < modifiedAdditionalChromosomesOrder.Count; i++)
+                {
+                    AddChromosomeToCell(childCell, modifiedAdditionalChromosomesOrder[i]);
+
+                    if (childCell.TotalMi == KnapsackMaxWeight)
+                        return childCell;
+                }
+            }
+
+            return childCell;
+        }
+
+        private void DoFitness(List<Cell> population)
+        {
+            population.Sort();
+        }
+
+        private void AddChromosomeToCell(Cell cell, Chromosome chromosome)
+        {
+            if (cell.TotalMi + chromosome.Mi <= KnapsackMaxWeight)
+            {
+                cell.Chromosomes.Add(chromosome);
+                cell.TotalMi += chromosome.Mi;
+                cell.TotalCi += chromosome.Ci;
+            }
+        }
+
+        private void SwapChromosomes(List<Chromosome> chromosomes, int firstIndex, int lastIndex)
+        {
+            if (chromosomes.Count > 0)
+            {
+                var temp = chromosomes[firstIndex];
+                chromosomes[firstIndex] = chromosomes[lastIndex];
+                chromosomes[lastIndex] = temp;
+            }
+        }
+    }
+}
---
+++ KnapsackProblem/InputDataTransformer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnapsackProblem
+{
+    public class InputDataTransformer
+    {
+        public static List<Chromosome> TransformInputData(string input)
+        {
+            //Converting the unput data
+            List<Chromosome> inputItems = new List<Chromosome>();
+            string[] inputs = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (var currentInput in inputs)
+            {
+                string[] parts = currentInput.Split(' ');
+                inputItems.Add(new Chromosome()
+                {
+                    Mi = Double.Parse(parts[0]),
+                    Ci = Double.Parse(parts[1])
+                });
+            }
+            return inputItems;
+        }
+    }
+}
